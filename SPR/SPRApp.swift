81f2b7c2680bebb6943f33f8702226baaa4f0ada
Daigo Shitara <stain.witness+github@gmail.com>
2021-06-19 10:04:45 +0900

    Initial Commit

---
+++ SPR/SPRApp.swift
@@ -0,0 +1,17 @@
+//
+//  SPRApp.swift
+//  SPR
+//
+//  Created by shitara on 2021/06/19.
+//
+
+import SwiftUI
+
+@main
+struct SPRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
